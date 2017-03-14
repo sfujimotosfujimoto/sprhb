d8e48f89a3a5d4998edd29eca0c05fd4da69c234
S Fujimoto <unicoshun@gmail.com>
2017-03-14 17:44:45 +0900

    ~#132

--- spring-mvc-demo/out/artifacts/spring_mvc_demo_war_exploded/WEB-INF/view/student-form.jsp
+++ spring-mvc-demo/out/artifacts/spring_mvc_demo_war_exploded/WEB-INF/view/student-form.jsp
@@ -4,6 +4,7 @@
 
 <html>
 <head>
+	<title>Student Registration Form</title>
 	
 </head>
 
@@ -14,6 +15,31 @@
 		<br><br>
 		
 		Last name: <form:input path="lastName" />
+		
+		<br><br>
+		
+		Country:
+		
+		<form:select path="country">
+			<form:options items="${student.countryOptions}" />
+		</form:select>
+		
+		<br><br>
+		
+		Favorite Language:
+		
+		Java <form:radiobutton path="favoriteLanguage" value="Java" />
+		C#<form:radiobutton path="favoriteLanguage" value="C#" />
+		PHP <form:radiobutton path="favoriteLanguage" value="PHP" />
+		Ruby <form:radiobutton path="favoriteLanguage" value="Ruby" />
+		
+		<br><br>
+		
+		Operating Systems:
+		
+		Linux <form:checkbox path="operatingSystems" value="Linux" />
+		Mac OS <form:checkbox path="operatingSystems" value="Mac" />
+		MS Windows <form:checkbox path="operatingSystems" value="MS Windows" />
 		
 		<br><br>
 		
--- spring-mvc-demo/src/com/sfujimoto/Student.java
+++ spring-mvc-demo/src/com/sfujimoto/Student.java
@@ -1,15 +1,31 @@
 package com.sfujimoto;
 
+import java.util.LinkedHashMap;
+
 /**
  * Created by sfujimoto on 2017/03/14.
  */
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
+    private LinkedHashMap<String, String> countryOptions;
+
+    private String favoriteLanguage;
+
+    private String[] operatingSystems;
 
     public Student() {
 
+        // populate country options: use ISO country code
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("US", "United States of America");
     }
 
     public String getFirstName() {
@@ -28,4 +44,31 @@ public class Student {
         this.lastName = lastName;
     }
 
+     public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String[] getOperatingSystems() {
+        return operatingSystems;
+    }
+
+    public void setOperatingSystems(String[] operatingSystems) {
+        this.operatingSystems = operatingSystems;
+    }
 }
